ff20bdf867b84bab4471bf8acdce41d0d140b5dd
a.ukhov <ukhov.164@gmail.com>
2025-05-09 23:39:58 +0400

    Initial commit

---
+++ src/main/kotlin/ssu/java/hungryStudent/HungryStudentApplication.kt
@@ -0,0 +1,11 @@
+package ssu.java.hungryStudent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HungryStudentApplication
+
+fun main(args: Array<String>) {
+	runApplication<HungryStudentApplication>(*args)
+}
---
+++ src/test/kotlin/ssu/java/hungryStudent/HungryStudentApplicationTests.kt
@@ -0,0 +1,13 @@
+package ssu.java.hungryStudent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HungryStudentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
